628bd72e55690d2f5c58c116341ac46d0fb25a92
Tiago Santos <tiago@convertodigital.com>
2020-03-03 18:58:02 +0000

    fixed nif acceptance and validation

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,5 +6,5 @@ class Profile < ApplicationRecord
   validates :last_name, presence: true
   validates :username, presence: true
   validates :bio, length: { maximum: 500 }
-  # validates :nif, length: { maximum: 9 }, numericality: { only_integer: true }
+  validates :nif, length: { is: 9 }, numericality: { only_integer: true }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_143642) do
+ActiveRecord::Schema.define(version: 2020_03_03_183132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2020_03_03_143642) do
     t.string "username"
     t.bigint "user_id"
     t.date "date_of_birth"
-    t.string "nif"
+    t.integer "nif"
     t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
