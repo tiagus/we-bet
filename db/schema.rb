3d137958f914ade415a57866108defab2ccb1baa
ricardo silva <rcorreiadasilva@outlook.com>
2020-03-03 14:43:51 +0000

    font color

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_150507) do
+ActiveRecord::Schema.define(version: 2020_03_03_111640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bets", force: :cascade do |t|
+    t.integer "numbers", null: false, array: true
+    t.integer "stars", null: false, array: true
+    t.boolean "milhao", default: false
+    t.integer "number_of_weeks", default: 1
+    t.boolean "auto_renew", default: false
+    t.string "status", null: false, array: true
+    t.bigint "user_id"
+    t.bigint "group_id"
+    t.bigint "draw_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["draw_id"], name: "index_bets_on_draw_id"
+    t.index ["group_id"], name: "index_bets_on_group_id"
+    t.index ["user_id"], name: "index_bets_on_user_id"
+  end
+
+  create_table "draws", force: :cascade do |t|
+    t.integer "numbers"
+    t.integer "stars"
+    t.date "draw_date", null: false
+    t.jsonb "prizes"
+    t.date "closing_date", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.boolean "public", default: true
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +64,8 @@ ActiveRecord::Schema.define(version: 2020_03_02_150507) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bets", "draws"
+  add_foreign_key "bets", "groups"
+  add_foreign_key "bets", "users"
+  add_foreign_key "groups", "users"
 end
