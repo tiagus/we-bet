3cc191fb9f70badf3a88f5f939d1b41f60daea70
marco varela <marco.varela12@gmail.com>
2020-03-07 12:24:17 +0000

    minor fixes

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,7 +2,6 @@ class ConversationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.where.not(id: current_user.id)
     @conversations = Conversation.where("sender_id = ? OR receiver_id = ?", current_user.id, current_user.id)
   end
 
