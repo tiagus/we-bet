ad9fa2437ce1678ed30ee724b4256f9d15a1c2fd
ricardo silva <rcorreiadasilva@outlook.com>
2020-03-09 10:22:39 +0000

    update

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 2020_03_06_230513) do
   end
 
   create_table "draws", force: :cascade do |t|
-    t.integer "numbers", array: true
+    t.integer "numbers"
     t.integer "stars"
     t.date "draw_date", null: false
     t.jsonb "prizes"
