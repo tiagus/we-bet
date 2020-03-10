3e682a26347d8b0609b67aebd0d2d2226b8d475d
marco varela <marco.varela12@gmail.com>
2020-03-10 18:11:52 +0000

    before installing web-hooks

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,26 @@
 class OrdersController < ApplicationController
+  def create
+    bet = Bet.find(params[:bet_id])
+    order  = Order.create!(bet: bet, amount: bet.price, state: 'pending', user: current_user)
+
+    session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      line_items: [{
+        name: bet.sku,
+        images: [bet.photo_url],
+        amount: bet.price_cents,
+        currency: 'eur',
+        quantity: 1
+      }],
+      success_url: order_url(order),
+      cancel_url: order_url(order)
+    )
+
+    order.update(checkout_session_id: session.id)
+    redirect_to new_order_payment_path(order)
+  end
+
+  def show
+    @order = current_user.orders.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,13 @@ Rails.application.routes.draw do
   patch 'dashboard', to: 'profiles#update'
   get 'checkout', to: 'pages#checkout'
   get 'thankyou', to: 'pages#thankyou'
-  resources :orders, only: [:show, :create]
   resource :profile, only: [:new, :create]
   resources :bets, only: [:create]
   resources :groups do
     resources :messages, only: [:index, :create]
     end
+  resources :orders, only: [:show, :create] do
+    resources :payments, only: :new
+    end
+  mount StripeEvent::Engine, at: '/stripe-webhooks'
 end
