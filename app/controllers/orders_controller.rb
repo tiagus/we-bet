ae8a607ec3dbc72bbe14c7659630b8a13b28ccac
marco varela <marco.varela12@gmail.com>
2020-03-12 18:05:17 +0000

    fixed stripe and created thank you page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,13 +6,13 @@ class OrdersController < ApplicationController
     session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
       line_items: [{
-        name: bet.numbers.to_s,
+        name: "Ticket: #{bet.numbers.join(' - ')} #{bet.stars.join(' - ')}",
         amount: bet.price_cents,
         currency: 'eur',
         quantity: 1
       }],
-      success_url: order_url(order),
-      cancel_url: order_url(order)
+      success_url: thankyou_url,
+      cancel_url: dashboard_url
     )
 
     order.update(checkout_session_id: session.id)
